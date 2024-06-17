f1b530c8c6c7f68ec2f0f833a07f0b6ddf0c6115
Drew Zucker <drewzucker1@gmail.com>
2024-06-17 15:28:09 -0400

    republish to npm and reorg file structure

--- src/lib/angular-trackstar-link.component.ts
+++ src/lib/angular-trackstar-link.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 type AllowedEndpoints = "get_inventory" | "get_products" | "create_product" | "update_product" | "get_inbound_shipment" | "create_inbound_shipment"
   | "update_inbound_shipment" | "get_orders" | "create_order" | "update_order" | "create_kit" | "update_kit" | "get_returns" | "create_return" | "update_return"
   | "get_shipping_methods" | "get_bills" | "get_warehouses" | "get_warehouse_customers" | "get_labor_activities";
-
+  
 interface ClientConfig {
   onSuccess: (authCode: string) => void;
   onClose?: () => void;
